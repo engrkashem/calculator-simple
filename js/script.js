e3ed3656ec515cfdb985d75f3ba999ae8a4793d8
Engr Mohammad Abul Kashem <kashemaust@gmail.com>
2022-02-12 11:11:00 +0600

    clicked numbers can display accordingly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const numbersBtn = document.getElementsByClassName('btn-number')
+function getDisplay(pressedNumber) {
+    const display = document.getElementById('display')
+    const previousTexDisplay = display.innerText;
+    const currentTextDisplay = previousTexDisplay + pressedNumber;
+    display.innerText = currentTextDisplay;
+}
+const numbersBtn = document.getElementsByClassName('btn-number');
+for (const numberBtn of numbersBtn) {
+    numberBtn.addEventListener('click', function () {
+        const numberBtnText = numberBtn.innerText;
+        getDisplay(numberBtnText)
+    })
+}
