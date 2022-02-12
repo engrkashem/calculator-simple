8510300aa81383320ba5dd117005f11951540d02
Engr Mohammad Abul Kashem <kashemaust@gmail.com>
2022-02-12 18:28:00 +0600

    fokira marka calculator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-function getDisplay(pressedNumber) {
-    const display = document.getElementById('display')
-    const previousTexDisplay = display.innerText;
-    const currentTextDisplay = previousTexDisplay + pressedNumber;
-    display.innerText = currentTextDisplay;
+function clr() {
+    const value = document.getElementById('display');
+    value.innerText = '';
 }
-const numbersBtn = document.getElementsByClassName('btn-number');
-for (const numberBtn of numbersBtn) {
-    numberBtn.addEventListener('click', function () {
-        const numberBtnText = numberBtn.innerText;
-        getDisplay(numberBtnText)
-    })
+function display(text) {
+    const currentValue = document.getElementById('display');
+    currentValue.innerText += text;
+    return currentValue;
+}
+function solve() {
+    const inputs = document.getElementById('display').innerText;
+    const result = eval(inputs);
+    document.getElementById('display').innerText = result;
 }
